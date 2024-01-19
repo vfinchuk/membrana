08d422febe0ff59ee4476152696f6f22040e78b2
v.finchuk <finchukvaldimir@gmail.com>
2024-01-19 22:33:42 +0100

    fox -update js

--- foc/ym-init.js
+++ foc/ym-init.js
@@ -1,20 +1,9 @@
 (function() {
-    const loadScript = function(url) {
-        const script = document.createElement('script');
-        document.head.appendChild(script);
-        script.async = true;
-        script.src = url;
-    };
-
-    const addSlotContainer = function(className = '') {
-        const container = document.createElement('div');
-        container.classList.add(className);
-        document.body.appendChild(container);
-    }
+    const container = document.createElement('FOC_VIDEO');
+    container.classList.add(className);
+    document.body.appendChild(container);
 
     addSlotContainer('FOC_VIDEO');
-
-    loadScript('https://cdn.membrana.media/foc/ym.js');
 
     window.notsyCmd = window.notsyCmd || [];
     window.notsyCmd.push(function () {
